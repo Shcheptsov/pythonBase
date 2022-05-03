faeef254bfae50ff9074ac092626699dd1b1a9df
shche <shcheptsovall@gmail.com>
2022-05-03 17:47:05 +0300

    lesson 3 07 completed(wall)

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
 
 # (цикл for)
-import simple_draw
+import simple_draw as sd
 
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
-# TODO здесь ваш код
+for current_y in range(12):
+    for current_x in range(7):
+        offset = 0 if current_y % 2 else 50
+        bottom_left = sd.get_point(offset + current_x * 100 - 50, 0 + current_y * 50)
+        top_right = sd.get_point(offset + 50 + current_x * 100, 50 + current_y * 50)
+        sd.rectangle(bottom_left, top_right, color=sd.COLOR_DARK_RED, width=3)
 
-simple_draw.pause()
+sd.pause()
+
+sd.pause()
